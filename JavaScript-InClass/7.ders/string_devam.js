aaa70bf00881d833ccd9d8966cbd1658c32e97f8
1ss1 <aybilenden@gmail.com>
2022-02-03 22:00:21 +0300

    string sonu 7. ders

---
+++ JavaScript-InClass/7.ders/string_devam.js
@@ -0,0 +1,7 @@
+var s =
+  "Lorem Ipsum is simply dummy text of the printing and 'printing' typesetting industry.";
+
+  console.log(s.replace('dummy', 'hello'));
+  //orijinal string değişmez. 
+
+ 
