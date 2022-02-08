e0bdbe4a3c834036d1187ff795ae202bc7f67fe0
1ss1 <aybilenden@gmail.com>
2022-02-08 23:27:34 +0300

    9. lesson events

---
+++ JavaScript-InClass/9.ders/DOM2.js
@@ -0,0 +1,11 @@
+/* 
+document.querySelector ilk bulduğunu getirir.
+document.querySelectorAll ise node list olarak sayfadaki tüm uyan sonuçları getirir
+*/
+/* 
+document.getElementsByClassName, document.getElementsByTagName ==> Html Collection
+
+document.getElementsByName, document.querySelectorAll ==> NodeList
+
+document.getElementById, document.querySelector ==> tek element */
+
---
+++ JavaScript-InClass/9.ders/FS_10_JS_EVENTS/signup.js
@@ -0,0 +1,79 @@
+// DOM recap, DOM traversing
+
+// let navbarPathItemText = document.getElementsByClassName("nav__item")[0].innerText;
+// console.log(navbarPathItemText);
+
+// let navbarPathItemText2 = document.getElementsByClassName("nav__link")[0].innerText;
+// console.log(navbarPathItemText2);
+
+// let navbarPathItemText3 = document.getElementsByClassName("nav__links")[0].children[0].innerText;
+// console.log(navbarPathItemText3);
+
+// let navbarPathItemText4 = document.getElementsByClassName("nav__links")[0].firstElementChild.innerText;
+// console.log(navbarPathItemText4);
+
+let navbarPathsLi = document.querySelector(".nav__item");
+console.log(navbarPathsLi);
+
+// window.addEventListener("event", func());
+window.addEventListener("load", () => {
+  console.log("window loaded");
+});
+
+// window.onload = func()
+window.onload = () => {
+  console.log("window onloaded");
+};
+
+navbarPathsLi.addEventListener("click", (e) => {
+  alert(navbarPathsLi.innerText + " clicked");
+});
+
+// 3. eventlistener ekleme şekli ödev
+
+// let navbarPathsLi2 = document.querySelector(".nav__links").firstElementChild;
+// console.log("ul -> first li", navbarPathsLi2.innerText);
+
+// let navbarSignUpLi = document.querySelector(".nav__links").lastElementChild;
+// console.log("ul -> last li", navbarSignUpLi.innerText);
+
+let navbarInstructorsLi = document.querySelectorAll(".nav__item")[1];
+console.log(navbarInstructorsLi);
+
+navbarInstructorsLi.addEventListener("mouseover", () => {
+  navbarInstructorsLi.innerText = "Over Me!";
+  navbarInstructorsLi.style.color = "green";
+  navbarInstructorsLi.style.fontSize = "1.7rem";
+});
+
+navbarInstructorsLi.addEventListener("mouseout", () => {
+  navbarInstructorsLi.innerText = "Instructors";
+  navbarInstructorsLi.style.color = "#444444";
+  navbarInstructorsLi.style.fontSize = "1.7rem";
+});
+
+let navbarStudentsLi = document.querySelectorAll(".nav__item")[2];
+console.log(navbarStudentsLi);
+
+// // // 🔥🔥🔥🔥🔥  rgb rndom color(rgb(255,255,255) onclick of Students Navbar (CODE CHALLENGE)  🔥🔥🔥🔥🔥
+
+const randomNumber = () => Math.round(Math.random() * 255);
+const randomColor = () => `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+
+//li
+navbarStudentsLi.addEventListener("click", (event)=>{
+    //stop bubbling
+    event.stopPropagation();
+    navbarStudentsLi.style.backgroundColor = randomColor();
+});
+
+//ul
+navbarStudentsLi.parentElement.addEventListener("click", (event)=>{
+    event.stopPropagation();
+    navbarStudentsLi.parentElement.style.backgroundColor = randomColor();
+});
+
+//header
+navbarStudentsLi.parentElement.parentElement.addEventListener("click", (event)=>{
+    navbarStudentsLi.parentElement.parentElement.style.backgroundColor = randomColor();
+});
