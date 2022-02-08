0b9b80c7aecf4fbb794c70460200a2e963fb997d
1ss1 <aybilenden@gmail.com>
2022-02-08 23:28:10 +0300

    events konu özeti

---
+++ JavaScript-InClass/DOM/DOM_events.js
@@ -0,0 +1,65 @@
+/* -------- JavaScript HTML DOM Events --------- */
+
+/* 
+Sık kullanılan DOM eventleri : 
+
+● onabort - Loading of an image is interrupted
+
+● onblur - An element loses focus
+
+● onchange - The content of a field changes
+
+● onclick - Mouse clicks an object
+
+● ondblclick - Mouse double-clicks an object
+
+● onerror - An error occurs when loading a document or an image
+
+● onfocus - An element gets focus
+
+● onkeydown - A keyboard key is pressed
+
+● onkeypress - A keyboard key is pressed or held down
+
+● onkeyup - A keyboard key is released
+
+● onload - A page or an image is finished loading
+
+● onmousedown - A mouse button is pressed
+
+● onmousemove - The mouse is moved
+
+● onmouseout - The mouse is moved off an element
+
+● onmouseover - The mouse is moved over an element
+
+● onmouseup - A mouse button is released
+
+● onreset - The reset button is clicked
+
+● onresize - A window or frame is resized
+
+● onselect - Text is selected
+
+● onsubmit - The submit button is clicked
+
+● onunload - The user exits the page
+*/
+
+/* 
+onload ve onunload >>> kullanıcı sayfaya girdiğinde veya sayfadan ayrıldığında tetiklenir.
+
+Onload olayı, ziyaretçinin tarayıcı türünü ve tarayıcı sürümünü kontrol etmek ve bilgilere dayalı olarak web sayfasının uygun sürümünü yüklemek için kullanılabilir.
+
+onload ve onunload olayları, tanımlama bilgileriyle başa çıkmak için kullanılabilir.(cookies)
+*/
+
+/* 
+.onchange >> genellikle input alanının onaylanmasıyla tetiklenir. (lowercase ile girilen girdiyi uppercase yapmak gibi)
+.onmauseover >>
+.onmauseout >>
+
+.onmausedown >> fareyle tıklandığında
+.onmauseup  >> fare düğmesi bırakıldığında
+.onclick    >> son olarak tıklama tamamlandığında tetiklenir.
+*/
