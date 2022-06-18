069091ec841d923476ebe89c9490953d6b8953bf
Sule <aybilenden@gmail.com>
2022-06-18 14:07:31 +0300

    added

--- DJANGO/024_DRF_Auth/users/urls.py
+++ DJANGO/024_DRF_Auth/users/urls.py
@@ -1,7 +1,11 @@
+
 from django.urls import path
 
-from users.views import RegisterView
+from users.views import RegisterView, logout
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', views.obtain_auth_token, name='login'),
+    path('logout/', logout, name='logout')
 ]
